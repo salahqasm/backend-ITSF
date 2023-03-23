519a9ac15bf08cdf9c4e60e9c68bad854f092d88
salah <salahqasm@outlook.com>
2023-03-23 21:33:17 +0300

    test

--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,7 @@ const feedback = require("./feedback.model.js");
 const studentModel = require("./student.model.js");
 const companyModel = require("./company.model.js");
 const taskModel = require("./task.model.js");
-const taskReq=require("./taskRequest.model.js");
+const taskReqModel=require("./taskRequest.model.js");
 const skillModel=require("./skill.model.js");
 const studentskillsModel=require("./studentSkills.model.js")
 const psql = process.env.DATABASE_URL
@@ -26,6 +26,7 @@ const company= companyModel(sequelize, DataTypes);
 const task=taskModel(sequelize, DataTypes);
 const skill=skillModel(sequelize,DataTypes);
 const studentskills=studentskillsModel(sequelize,DataTypes);
+const req=taskReqModel(sequelize,DataTypes);
 
 student.belongsToMany(skill,{through:studentskills});
 skill.belongsToMany(student,{through:studentskills})
@@ -36,25 +37,17 @@ task.belongsTo(company);
 student.hasMany(task,{onDelete:"SET NULL",onUpdate:'CASCADE'});
 task.belongsTo(student);
 
-
-//relationships 
-// const shop=shopModel(sequelize,DataTypes);
-// const customer=customerModel(sequelize,DataTypes);
-// const order=orderModel(sequelize,DataTypes);
-// const owner=ownerModel(sequelize,DataTypes)
+student.belongsToMany(task,{through:req,as:"req",onDelete:"CASCADE"});
+task.belongsToMany(student,{through:req,as:"req"})
 
 
-// shop.belongsToMany(customer,{through:order});
-// customer.belongsToMany(shop,{through:order});
-// owner.hasOne(shop);
-// shop.belongsTo(owner);
 
 module.exports = {
   sequelize: sequelize,
   users: users(sequelize, DataTypes),
   doctor: doctor(sequelize, DataTypes),
   feedback: feedback(sequelize, DataTypes),
-  taskReq:taskReq(sequelize,DataTypes),
+  req,
   studentskills,
   skill,
   student,
