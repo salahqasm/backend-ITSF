232944aad6e95160637470d5b3d5bf592e392bec
salah <salahqasm@outlook.com>
2023-05-15 23:26:17 +0300

    gettasks

--- src/routes/admin-routes/admin-get.js
+++ src/routes/admin-routes/admin-get.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { company, student, doctor, users } = require("../../models/index.js");
+const { company, student, doctor, users, task } = require("../../models/index.js");
 const bearerauth = require("../../middlewares/bearer.js")
 const aclMiddleware = require('../../middlewares/acl.js');
 const router = express.Router();
@@ -8,7 +8,7 @@ router.use(bearerauth);
 router.get('/allstudents', aclMiddleware('admin'), getAllStudents)
 router.get('/allcompanies', aclMiddleware('admin'), getAllCompanies)
 router.get('/alldoctors', aclMiddleware('admin'), getAllDoctors)
-
+router.get('/alltasks', getAllTasks)
 async function getAllStudents(req, res) {
     let userD = await student.findAll();
     res.status(200).json(userD);
@@ -27,6 +27,20 @@ async function getAllDoctors(req, res) {
 
 }
 
+async function getAllTasks(req, res) {
+    try {
+        const result = await task.findAll({
+            include: [
+                { model: student, attributes: { exclude: ['password'] } },
+                { model: company, attributes: { exclude: ['password'] } }
+            ]
+        })
+        res.send(result);
+    } catch (err) {
+        console.log(err.message);
+        res.send(err.message);
+    }
+}
 
 
 module.exports = router;
